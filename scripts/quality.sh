106902412adbeaac0064bf59fb67973a02445cba
Carlos Giraldo <carlgira@MacBook-Pro-de-Carlos.local>
2019-04-04 23:27:38 +0200

    quality scripts

--- scripts/quality.sh
+++ scripts/quality.sh
@@ -8,14 +8,18 @@ mkdir -p quality
 SAMPLE_FILE=$1
 
 FASTA_FILES=($(awk 'NR>1{print $1}' $SAMPLE_FILE))
+SAMPLE_NAMES=($(awk 'NR>1{print $3}' $SAMPLE_FILE))
 
 echo "*** [$(date)] [quality.sh] Fastqc $SAMPLE_FILE"
 for index in ${!FASTA_FILES[*]};
  do
    FASTA=${FASTA_FILES[$index]}
-   if [ ! -f $FASTA ]; then
-     mkdir -p /work/quality/${FASTA}
-     fastqc -o /work/quality/${FASTA} $FASTA
+   SAMPLE_NAME=${SAMPLE_NAMES[$index]}
+   Q_DIR=/work/quality/${SAMPLE_NAME}
+
+   if [ ! -f $Q_DIR ]; then
+     mkdir -p $Q_DIR
+     fastqc -o /work/quality/${SAMPLE_NAME} $FASTA
    fi
 done
 
